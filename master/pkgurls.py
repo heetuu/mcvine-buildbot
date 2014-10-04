ac08a273143ec6b599cc10ec594d971fa56efafb
Jiao Lin <linjiao@caltech.edu>
2014-10-04 13:49:06 -0700

    mostly working. buildbot version: 0.8.9

---
+++ master/pkgurls.py
@@ -0,0 +1,23 @@
+# this is created by command
+#
+#  $ getrepourls.py pylist
+#
+# in the mcvine releaser
+
+svn_urls = [
+'svn://danse.us/ctrl/config/branches/with_doxygen_and_docbook_support' ,
+'svn://danse.us/pyre/pythia-0.8' ,
+'svn://danse.us/pyregui/trunk',
+'svn://danse.us/instrument/trunk' ,
+'svn://danse.us/common/dataObjects/sample/branches/sampleassembly' ,
+'svn://danse.us/inelastic/crystal/matter' ,
+'svn://danse.us/inelastic/dsm',
+'svn://danse.us/inelastic/numpyext',
+'svn://danse.us/inelastic/bpext',
+'svn://danse.us/MCViNE/trunk',
+'svn://danse.us/sans/trunk/sansmodels/src',
+]
+
+git_urls = [
+'git@github.com:yxqd/histogram.git',
+]
